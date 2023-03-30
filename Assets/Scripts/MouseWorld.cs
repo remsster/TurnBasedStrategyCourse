83c762f04fa83a3305b634206e004460087e290a
Remi Tobias <remitobias1@outlook.com>
2023-03-30 22:44:49 +0100

    Mouse World Position

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -4,13 +4,34 @@ namespace TurnBaseStrategy.Core
 {
     public class MouseWorld : MonoBehaviour
     {
+        [SerializeField] private LayerMask mousePlaneLayerMask;
+
+        private static MouseWorld instance;
+
+        // ----------------------------------------------------------------------------
+        // Unity Enging Methods
+        // ----------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
         private void Update()
         {
-            
+            transform.position = MouseWorld.GetPosition();
+        }
 
+        // ----------------------------------------------------------------------------
+        // Custom Methods
+        // ----------------------------------------------------------------------------
+
+        public static Vector3 GetPosition() 
+        {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            Debug.Log(Physics.Raycast(ray));
+            Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+            // if the raycast doesnt hit anything then it returns Vector3(0,0,0)
+            return raycastHit.point;
         }
     }
 }
