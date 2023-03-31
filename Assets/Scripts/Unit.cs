2f807aa03174475bb61345813f1a54e8ace773be
Remi Tobias <remitobias1@outlook.com>
2023-03-31 16:27:32 +0100

    Unit Animator Parameters

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,8 +5,11 @@ namespace TurnBaseStrategy.Core
     public class Unit : MonoBehaviour
     {
 
+
         [SerializeField] private float moveSpeed = 4f;
         [SerializeField] private float stoppingDistance = .1f;
+
+        [SerializeField] private Animator unitAnimator;
 
         private Vector3 targetPosition;
 
@@ -17,10 +20,16 @@ namespace TurnBaseStrategy.Core
 
         private void Update()
         {
+
             if (Vector3.Distance(transform.position,targetPosition) > stoppingDistance)
             {
                 Vector3 moveDirection = (targetPosition - transform.position).normalized;
                 transform.position += moveDirection * moveSpeed * Time.deltaTime;
+                unitAnimator.SetBool("IsWalking", true);
+            }
+            else
+            {
+                unitAnimator.SetBool("IsWalking", false);
             }
 
             if (Input.GetMouseButtonDown(0))
