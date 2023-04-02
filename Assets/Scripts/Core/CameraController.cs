79dd278fcc4f511064cd041982c4d3218200bfec
Remi Tobias <remitobias1@outlook.com>
2023-04-02 13:02:28 +0100

    Camera Zoom

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 
 namespace TurnBaseStrategy.Core
@@ -6,10 +7,40 @@ namespace TurnBaseStrategy.Core
     {
         [SerializeField] private float moveSpeed = 10f;
         [SerializeField] private float rotationSpeed = 100f;
+        [SerializeField] private float zoomSpeed = 5f;
+
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+        private Vector3 targetFollowOffset;
+
+        private const float minFollowYOffset = 2f;
+        private const float maxFollowYOffset = 12f;
+        private CinemachineTransposer cinemachineTransposer;
+
+        // ----------------------------------------------------------------------------
+        // Unity Enging Methods
+        // ----------------------------------------------------------------------------
+
+        private void Start()
+        {
+            cinemachineTransposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+            targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+        }
 
         private void Update()
         {
-            Vector3 inputMoveDirection = new Vector3(0,0,0);
+            HandleMovement();
+            HandleRotation();
+            HandleZoom();
+        }
+
+        // ----------------------------------------------------------------------------
+        // Custom Methods
+        // ----------------------------------------------------------------------------
+
+        private void HandleMovement() 
+        {
+            Vector3 inputMoveDirection = new Vector3(0, 0, 0);
             if (Input.GetKey(KeyCode.W))
             {
                 inputMoveDirection.z = +1f;
@@ -29,7 +60,10 @@ namespace TurnBaseStrategy.Core
 
             Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
             transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
 
+        private void HandleRotation()
+        {
             Vector3 rotationVector = new Vector3(0, 0, 0);
 
             if (Input.GetKey(KeyCode.Q))
@@ -42,7 +76,25 @@ namespace TurnBaseStrategy.Core
             }
 
             transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+        }
 
+        private void HandleZoom()
+        {
+            float zoomAmount = 1f;
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                // zoom in
+                targetFollowOffset.y -= zoomAmount;
+            }
+
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                // zoom out
+                targetFollowOffset.y += zoomAmount;
+            }
+
+            targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, minFollowYOffset, maxFollowYOffset);
+            cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
         }
     }
 }
